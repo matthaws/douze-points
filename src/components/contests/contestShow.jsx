2a9aa69bb931d8088a481bed4c86d39679ea9690
Ian Del Duca <ianmdelduca@gmail.com>
2018-03-18 15:39:53 -0400

    update

--- src/components/contests/contestShow.jsx
+++ src/components/contests/contestShow.jsx
@@ -27,64 +27,8 @@ class ContestShow extends React.Component {
 
   render() {
     const { contest, entries, countries } = this.props;
-    if (countries.length < 1 || entries.includes(undefined) ) {
-      return null;
-    } else {
-      return (
-        <main className="main--contestShowPage">
-          <div className="div--contest-title">
-            EuroVision Song Contest {contest.year}
-          </div>
-          <ul className="ul--entries">
-            {entries.map(entry => {
-              const flag_url = countries[entry.country_id]
-                ? countries[entry.country_id].flag_url
-                : "";
 
-              let medal;
-              switch (entry.final_ranking) {
-                case 1:
-                  medal = (
-                    <img alt="gold-medal" src={Gold} className="img--medal" />
-                  );
-                  break;
-                case 2:
-                  medal = (
-                    <img alt="silver-medal" src={Silver} className="img--medal" />
-                  );
-                  break;
-                case 3:
-                  medal = (
-                    <img alt="bronze-medal" src={Bronze} className="img--medal" />
-                  );
-                  break;
-                default:
-                  medal = "";
-              }
-
-              return (
-                <Link to={`/entries/${entry.id}`}>
-                  <li className="li--entry">
-                    <img
-                      alt="country-flag"
-                      src={flag_url}
-                      className="img--flag"
-                    />
-                    <span className="span--entry">
-                      {entry.song_title}, {entry.artist}
-                    </span>
-                    {medal}
-                    <span className="span--rank">#{entry.final_ranking}</span>
-                  </li>
-                </Link>
-              );
-            })}
-          </ul>
-        </main>
-      );
-    }
-
-    return (
+  return (
       <main className="main--contestShowPage">
         <div className="div--contest-title">
           EuroVision Song Contest {contest.year}
