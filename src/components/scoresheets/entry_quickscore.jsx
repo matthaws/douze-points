11b2b5c9fb8789803373d58accf35aa6ec005cf7
Brendan Hamill <brenhamill@gmail.com>
2018-04-08 17:28:02 -0400

    add submit action to entry quickscore

--- src/components/scoresheets/entry_quickscore.jsx
+++ src/components/scoresheets/entry_quickscore.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createScoring, updateScoring } from '../../actions/scoringActions';
 
 class EntryQuickscore extends React.Component {
   constructor(props) {
@@ -21,6 +23,28 @@ class EntryQuickscore extends React.Component {
       entry_id: props.entry.id,
       scoresheet_id: '',
     };
+    this.submitScoring = this.submitScoring.bind(this);
+  }
+
+  handleChange(field) {
+    return (e) => {
+      this.setState({ [field]: e.target.value });
+    };
+  }
+
+  submitScoring(e) {
+    e.preventDefault();
+    const score = this.state;
+    Object.keys(score).forEach((key) => {
+      if (score[key] === '') {
+        score[key] = null;
+      }
+    });
+    if (score.id) {
+      this.props.updateScoring(score);
+    } else {
+      this.props.createScoring(score);
+    }
   }
 
   render() {
@@ -28,7 +52,8 @@ class EntryQuickscore extends React.Component {
       <div className="div--entry_quickscore_container">
         <p>Quickscore</p>
         <hr />
-        <form>
+        <form onSubmit={ this.submitScoring }>
+          <button onClick={this.submitScoring}>Submit</button>
           <label>Choose Scoresheet:
             <select
               name="scoresheet_id"
@@ -47,10 +72,75 @@ class EntryQuickscore extends React.Component {
               }
             </select>
           </label>
+          <table>
+            <tr>
+              <th>Song Score</th>
+              <th>Dance Score</th>
+              <th>Costume Score</th>
+              <th>Eurocheese Score</th>
+              <th>Bonus Points</th>
+            </tr>
+            <tr>
+              <td>
+                <input
+                  type="number"
+                  min="0"
+                  max="12"
+                  name="song_score"
+                  onChange={this.handleChange('song_score')}
+                  value={this.state.song_score || ''}
+                />
+              </td>
+              <td>
+                <input
+                  type="number"
+                  min="0"
+                  max="12"
+                  name="dance_score"
+                  onChange={this.handleChange('dance_score')}
+                  value={this.state.dance_score || ''}
+                />
+              </td>
+              <td>
+                <input
+                  type="number"
+                  min="0"
+                  max="12"
+                  name="costume_score"
+                  onChange={this.handleChange('costume_score')}
+                  value={this.state.costume_score || ''}
+                />
+              </td>
+              <td>
+                <input
+                  type="number"
+                  min="0"
+                  max="12"
+                  name="cheese_score"
+                  onChange={this.handleChange('cheese_score')}
+                  value={this.state.cheese_score || ''}
+                />
+              </td>
+              <td>
+                <input
+                  type="number"
+                  name="bonus_points"
+                  onChange={this.handleChange('bonus_points')}
+                  value={this.state.bonus_points || ''}
+                />
+              </td>
+            </tr>
+          </table>
         </form>
       </div>
     )
   }
 }
 
-export default EntryQuickscore;
+const mapDispatchToProps = dispatch => ({
+  createScoring: scoring => dispatch(createScoring(scoring)),
+  updateScoring: scoring => dispatch(updateScoring(scoring)),
+
+});
+
+export default connect(null, mapDispatchToProps)(EntryQuickscore);
